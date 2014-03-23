70a38a10d992b29bae50d7718752a33d67b00acb
David Przybilla <dav.alejandro@gmail.com>
2014-03-23 14:28:07 +0000

    added flask server

--- service.py
+++ service.py
@@ -1,61 +1,34 @@
 # -*- coding: utf-8 -*- 
-from news_sources import RssFeeder
-from VectorUtils import Vector
-from Cluster import Clustering
 
-feeders = list()
+import json
+
+from news_sources import RssFeeder, CaracolRadioRssSource, TiempoRssSource
+from flask import Flask, request, Response
+from EventSearcher import EventSearcher
+
+app = Flask(__name__)
+event_searcher = EventSearcher()
 
 def init_server():
 
     url_feeds = [
-                #"http://www.eltiempo.com/politica/rss.xml",
-                "http://www.caracol.com.co/feed.aspx?id=196"
+                TiempoRssSource("http://www.eltiempo.com/politica/rss.xml"),
+                CaracolRadioRssSource("http://www.caracol.com.co/feed.aspx?id=196")
                 ]
 
     for feed in url_feeds:
         feeder = RssFeeder(feed)
-        feeders.append(feeder)
-
-init_server()
-
-def get_events(word_vector, date_str):
-
-    articles = list()
-    vectors = list()
-
-    for feeder in feeders:
-        print "in feeders"
-        articles = articles + feeder.get_content(date_str)
-
-    print articles[0]
+        event_searcher.add_feedsource(feeder)
 
 
-    for article in articles:
-        vector = Vector(article['content'][0]['value']).get_vector()
-        vectors.append(vector)
-        print vector
+@app.route("/get_event")
+def get_event():
+    date = request.args['date']
+    words = json.loads(request.args['words'])
 
-    word_vector_text = ' '.join(word_vector)
-    flag_vector = Vector(word_vector_text).get_vector()
-    for key,value in flag_vector.items():
-        flag_vector[key] =  flag_vector[key] + 2
+    score, url = event_searcher.get_event(words, date)
+    return Response(json.dumps({'score':score, 'url':url}))
 
-    clustering = Clustering(vectors, flag_vector)
-
-    index, value = clustering.get_articles()
-
-    print articles[index]
-    print "Value:"
-    print "\t",value
-    print articles[index]['link']
-
-
-
-    # cluster articles with word_vector
-
-
-words ="Peñalosa, Peñalosa, Enrique, Enrique, registradura, registraduria, Peñalosa, CNE, candidato, verde".split(",")
-
-
-
-get_events(words, "2014-03-21 23:00:00")
+if __name__ == '__main__':
+    init_server()
+    app.run(host='0.0.0.0', port=5000)
